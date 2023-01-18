dfda6c8a1dc093447c8b1c2fb18ff4b5a0da37af
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-18 09:25:08 -0300

    (web): finished 2nd class

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
-function App() {
-  return <div>Hello World</div>
-}
+import { Home } from './pages/Home'
 
-export default App
+export function App() {
+  return <Home />
+}
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#09090A',
+      },
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))',
+      },
+    },
   },
   plugins: [],
 }
