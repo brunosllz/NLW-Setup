fba8b5966aff673c75ebe82cd7ded2b8287ef952
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-17 21:07:13 -0300

    (server): finished 2nd class

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,11 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors';
-import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
+
 const app = Fastify()
 
 app.register(cors)
-
-app.get('/hello', async () => {
-  const habits = await prisma.habit.findMany({})
-
-  return habits
-})
+app.register(appRoutes)
 
 app.listen({
   port: 3333
