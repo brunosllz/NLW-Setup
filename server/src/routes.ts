f39a4e495d7446d0d3374419638f5915035a500d
Bruno Luiz <brunoslvrlz13@gmail.com>
2023-01-20 19:20:33 -0300

    (server): return empty array on day route and add host prop to server

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -113,7 +113,7 @@ export async function appRoutes(app: FastifyInstance) {
 
     const completeHabits = day?.dayHabits.map(dayHabit => {
       return dayHabit.habit_id
-    })
+    }) ?? []
 
     return {
       possibleHabits,
--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,7 @@ app.register(cors)
 app.register(appRoutes)
 
 app.listen({
+  host: '0.0.0.0',
   port: 3333
 }).then(() => {
   console.log('Server Runnig! 🚀')
